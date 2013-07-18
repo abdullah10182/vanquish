c3c636fa791ae08d36069796605902fd5651a50d
abdullah <abdullah10182@gmail.com>
2013-07-18 08:41:21 -0400

    updated fx slideshow

--- js/custom.js
+++ js/custom.js
@@ -5,7 +5,7 @@
     jQuery('#camera_wrap_1').camera({
             thumbnails: false,
             pagination: false,
-            fx: "scrollBottom",
+            fx: "scrollBottom", 
             transPeriod: 2000,
             onEndTransition: function() {
                jQuery('#services').show().animate({bottom:'40px'},1000, function(){
