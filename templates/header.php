2af411e0639fca6a7fbf93c72079229fa6dfed31
abdullah <abdullah10182@gmail.com>
2013-08-02 11:01:08 -0400

    added google analytics

--- templates/header.php
+++ templates/header.php
@@ -34,7 +34,8 @@ if (count($matches)>1){
 }
 ?>
 <!DOCTYPE html>
-<html lang="en"><head>
+<html lang="en">
+<head>
   <meta charset="utf-8">
   <title>Vanquish Yachts | <?php echo ucfirst($page); ?></title>
   <meta name="keywords" content="Vanquish yachts">
@@ -76,10 +77,18 @@ if (count($matches)>1){
     <script src="/js/mb.bgndGallery.js"></script>
      <script src="/js/custom.js"></script>
 
-    
-
-
   <link rel="shortcut icon" href="http://www.vanquish-yachts.com/favicon.ico">
+  
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-42897876-1', 'vanquish-yachts.com');
+  ga('send', 'pageview');
+
+</script>
   
  
 </head>
