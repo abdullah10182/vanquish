65217c5f527b6174021f78f1358087017a29b9fb
abdullah <abdullah10182@gmail.com>
2013-09-15 18:37:41 -0400

    added new VQ80 images

--- pages/vq80.php
+++ pages/vq80.php
@@ -34,24 +34,48 @@
     <div class="row-fluid">
     <div class="span8" style="position:relative;">
 
-         <div class="camera_wrap camera_azure_skin" id="camera2">
+         <div class="camera_wrap camera_ash_skin" id="camera2">
       
-         <div class="product_image" data-thumb="/img/products/vq80-side-view-t.jpg" data-src="/img/products/vq80-side-view.jpg" >
+         <div class="product_image" data-thumb="/img/products/vq80/vq80-side-view-t.jpg" data-src="/img/products/vq80/vq80-side-view.jpg" >
              <div class="camera_caption fadeFromBottom">
-             Side view <a href="/img/products/vq80-side-view-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
+             Side view <a href="/img/products/vq80/vq80-side-view-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
              </div>
          </div>
       
-          <div class="product_image" data-thumb="/img/products/vq80-top-view-t.jpg" data-src="/img/products/vq80-top-view.jpg" >
+          <div class="product_image" data-thumb="/img/products/vq80/vq80-top-view-t.jpg" data-src="/img/products/vq80/vq80-top-view.jpg" >
              <div class="camera_caption fadeFromBottom">
-             Top view<a href="/img/products/vq80-top-view-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
+             Top view<a href="/img/products/vq80/vq80-top-view-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
+           </div>
+         </div>
+             
+         <div class="product_image" data-thumb="/img/products/vq80/vq80-view-01-t.jpg" data-src="/img/products/vq80/vq80-view-01.jpg" >
+             <div class="camera_caption fadeFromBottom">
+             VQ80<a href="/img/products/vq80/vq80-view-01-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
+           </div>
+         </div>
+             
+         <div class="product_image" data-thumb="/img/products/vq80/vq80-view-02-t.jpg" data-src="/img/products/vq80/vq80-view-02.jpg" >
+             <div class="camera_caption fadeFromBottom">
+             VQ80<a href="/img/products/vq80/vq80-view-02-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
+           </div>
+         </div>
+             
+         <div class="product_image" data-thumb="/img/products/vq80/vq80-view-03-t.jpg" data-src="/img/products/vq80/vq80-view-03.jpg" >
+             <div class="camera_caption fadeFromBottom">
+             VQ80<a href="/img/products/vq80/vq80-view-03-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
+           </div>
+         </div>
+             
+         <div class="product_image" data-thumb="/img/products/vq80/vq80-view-04-t.jpg" data-src="/img/products/vq80/vq80-view-04.jpg" >
+             <div class="camera_caption fadeFromBottom">
+             VQ80<a href="/img/products/vq80/vq80-view-04-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
            </div>
          </div>
          
       <?php /*
-           <div class="product_image" data-thumb="/img/products/vq80-top-sec-view-t.jpg" data-src="/img/products/vq40-top-sec-view.jpg" >
+           <div class="product_image" data-thumb="/img/products/vq80/vq80-top-sec-view-t.jpg" data-src="/img/products/vq80/vq40-top-sec-view.jpg" >
              <div class="camera_caption fadeFromBottom">
-            Top view interior <a href="/img/products/vq80-top-sec-view-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
+            Top view interior <a href="/img/products/vq80/vq80-top-sec-view-large.jpg" class="product_image view_large group"><i class=" icon-fullscreen"></i> View large</a>
            </div>
          </div>
 */
@@ -104,11 +128,11 @@
   
     
         jQuery('#camera2').camera({
-                height: '400px',
+                height: '50%',
                 loader: false,
                 pagination: false,
                 thumbnails: true,
-                navigation: false,
+                navigation: true,
                 playPause: false,
                 fx: 'scrollHorz'
   
