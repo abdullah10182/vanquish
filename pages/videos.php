adca31f9c4f95bc60ed0a805a263fe5d619b590e
abdullah <abdullah10182@gmail.com>
2013-10-17 11:34:44 -0400

    added video vq43

--- pages/videos.php
+++ pages/videos.php
@@ -1,5 +1,6 @@
 <?php 
-$yt_link="http://www.youtube.com/embed/dY0PGR9wiaI?rel=0&amp;wmode=transparent&amp;autoplay=1&amp;vq=large";
+$yt_link_vq32="http://www.youtube.com/embed/dY0PGR9wiaI?rel=0&amp;wmode=transparent&amp;autoplay=1&amp;vq=large";
+$yt_link_vq43="http://www.youtube.com/embed/U7GVAashAPQ?rel=0&amp;wmode=transparent&amp;autoplay=1&amp;vq=large";
 $yt_link_class ="yt_link";
 
    $path_header = $_SERVER['DOCUMENT_ROOT'];
@@ -32,17 +33,29 @@ $yt_link_class ="yt_link";
             <h1>Videos</h1>
         </div>
       </div>
-      
-        <div class="row-fluid">
-       <a class="<?php print $yt_link_class; ?>" href="<?php print $yt_link; ?>"> 
-        <div class="span6 video">
+        
+     <!-- VQ32 --> 
+     <div class="row-fluid">
+       <a class="<?php print $yt_link_class; ?>" href="<?php print $yt_link_vq32; ?>"> 
+        <div class="span6 video box_left">
           <img class='youtube'  src="../img/generic/video-vq32.jpg">
                <div class="video_title "><h3>Vanquish VQ32</h3>
         </div>
       </div> 
       </a>
-     
-      
+ 
+    
+    <!-- VQ43 -->
+  
+       <a class="<?php print $yt_link_class; ?>" href="<?php print $yt_link_vq43; ?>"> 
+        <div class="span6 video">
+          <img class='youtube'  src="../img/generic/video-vq43.jpg">
+               <div class="video_title "><h3>Vanquish VQ43</h3>
+        </div>
+      </div> 
+      </a>
+    </div>
+    
     </div>
   </section>  
   
